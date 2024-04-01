f7d994a1ac90b4356bc76675013f985637a054a4
reyarp85 <zytan1985@gmail.com>
2024-04-01 20:30:02 +0800

    ecs-fargate creation

---
+++ autoloadbalancer.tf
@@ -0,0 +1,26 @@
+resource "aws_lb" "ecs-alb" {
+  name               = "zyt-ecs-autoloadbalance"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.sg_id]
+  subnets            = toset(data.aws_subnets.existing_subnets.ids)
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "ecs-alb-tg" {
+  name        = "zyt-ecs-target-group"
+  target_type = "alb"
+  port        = 80
+  protocol    = "TCP"
+  vpc_id      = data.aws_vpc.existing_vpc.id
+  health_check {
+    path      = "/"
+    protocol  = "HTTP"
+    interval = 300
+  } 
+}
